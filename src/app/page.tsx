7ca609a4a5b608b0119e9c8bea04ac15103473d2
Arian <arian.islam024@gmail.com>
2023-05-09 13:25:04 +0600

    fix: added the css class to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import React from "react";
 const page = () => {
   return (
     <div className="home flex items-center justify-center h-screen">
-      <button className="self-center w-48 h-14 text-4xl font-extrabold bg-slate-100 rounded-xl">
-        <Link className="bg-transparent text-slate-800" href="/employees">
+      <button className="form-button">
+        <Link className="button-link" href="/employees">
           Employees
         </Link>
       </button>
