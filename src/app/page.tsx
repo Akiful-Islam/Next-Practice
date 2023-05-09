21412b0199e10db1492760debc3f031667998ff3
Arian <arian.islam024@gmail.com>
2023-05-09 13:07:59 +0600

    feat: centered and designed home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,13 @@ import React from "react";
 
 const page = () => {
   return (
-    <button>
-      <Link href="/employees">Employees</Link>
-    </button>
+    <div className="home flex items-center justify-center h-screen">
+      <button className="self-center w-48 h-14 text-4xl font-extrabold bg-slate-100 rounded-xl">
+        <Link className="bg-transparent text-slate-800" href="/employees">
+          Employees
+        </Link>
+      </button>
+    </div>
   );
 };
 
