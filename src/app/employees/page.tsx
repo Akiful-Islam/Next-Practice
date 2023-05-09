09139762955569f0bd9905679a5d1d9f517ecb9d
Arian <arian.islam024@gmail.com>
2023-05-09 13:25:18 +0600

    feat: added button style to employee page

--- src/app/employees/page.tsx
+++ src/app/employees/page.tsx
@@ -3,21 +3,31 @@ import React from "react";
 
 const page = () => {
   return (
-    <div className="emploee-root">
-      <button>
-        <Link href="/employees/get">Get All</Link>
+    <div className="employees-root flex flex-col items-center justify-center h-screen">
+      <button className="form-button">
+        <Link className="button-link" href="/employees/get">
+          Get All
+        </Link>
       </button>
-      <button>
-        <Link href="/employees/get/{id}">Get By Id</Link>
+      <button className="form-button">
+        <Link className="button-link" href="/employees/get/{id}">
+          Get By Id
+        </Link>
       </button>
-      <button>
-        <Link href="/employees/post">Post</Link>
+      <button className="form-button">
+        <Link className="button-link" href="/employees/post">
+          Post
+        </Link>
       </button>
-      <button>
-        <Link href="/employees/patch">Patch</Link>
+      <button className="form-button">
+        <Link className="button-link" href="/employees/patch">
+          Patch
+        </Link>
       </button>
-      <button>
-        <Link href="/employees/delete">Delete</Link>
+      <button className="form-button">
+        <Link className="button-link" href="/employees/delete">
+          Delete
+        </Link>
       </button>
     </div>
   );
