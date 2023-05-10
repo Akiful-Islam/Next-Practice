daad61a2044ed4f7706411ddeef078169e94c1e2
Arian <arian.islam024@gmail.com>
2023-05-10 16:07:15 +0600

    change: slight redesign in employee page

--- src/app/employees/page.tsx
+++ src/app/employees/page.tsx
@@ -5,25 +5,31 @@ import React from "react";
 
 const page = () => {
   return (
-    <Card>
-      <div className="employees-root flex flex-col">
-        <Button type="filled" className="page-button">
-          <Link href="/employees/get">Get All</Link>
-        </Button>
-        <Button type="transparent" className="page-button">
-          <Link href="/employees/get/{id}">Get by Id</Link>
-        </Button>
-        <Button type="filled" className="page-button">
-          <Link href="/employees/post">Create</Link>
-        </Button>
-        <Button type="transparent" className="page-button">
-          <Link href="/employees/patch/">Update</Link>
-        </Button>
-        <Button type="filled" className="page-button">
-          <Link href="/employees/delete/">Delete</Link>
-        </Button>
-      </div>
-    </Card>
+    <div className="flex flex-col">
+      <h1 className="">Employees</h1>
+      <Card>
+        <div className="employees-root flex flex-col w-80 h-96 justify-center">
+          <Button className="page-button">
+            <Link href="/employees/get">Get All</Link>
+          </Button>
+          <Button type="transparent" className="page-button">
+            <Link href="/employees/get/{id}">Get by Id</Link>
+          </Button>
+          <Button className="page-button">
+            <Link href="/employees/post">Create</Link>
+          </Button>
+          <Button type="transparent" className="page-button">
+            <Link href="/employees/patch/">Update</Link>
+          </Button>
+          <Button className="page-button">
+            <Link href="/employees/delete/">Delete</Link>
+          </Button>
+        </div>
+      </Card>
+      <Button className="place-self-end">
+        <Link href="/">Back</Link>
+      </Button>
+    </div>
   );
 };
 
