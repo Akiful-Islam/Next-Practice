a0eeef9aaa7a863bc0af0fc813405aa601bc565c
Arian <arian.islam024@gmail.com>
2023-05-10 16:19:08 +0600

    feat: start of EmployeeTable component

--- src/app/employees/(routes)/get/page.tsx
+++ src/app/employees/(routes)/get/page.tsx
@@ -1,11 +1,12 @@
 import GetAllPaginated from "@/components/crud/GetAllPaginated";
+import EmployeeTable from "@/components/data/EmployeeTable";
 import React from "react";
 
 const page = () => {
   return (
     <div className="get-all-root">
-      <h1 className="">Get All</h1>
-      <GetAllPaginated />
+      <h1 className="">Get All Employees</h1>
+      <EmployeeTable />
     </div>
   );
 };
