7def7e4a7cf2261f5f1a3aa96fd5ca66078c5a69
Arian <arian.islam024@gmail.com>
2023-05-10 16:06:50 +0600

    refactor: home page changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
+import Button from "@/components/input/Button";
 import Link from "next/link";
 import React from "react";
 
 const page = () => {
   return (
     <div className="home">
-      <button className="page-button">
+      <Button className="page-button">
         <Link className="button-link" href="/employees">
           Employees
         </Link>
-      </button>
+      </Button>
     </div>
   );
 };
