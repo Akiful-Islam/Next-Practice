332a4e7858aff1ce2c30d434b1f7f08a5ef0127e
Arian <arian.islam024@gmail.com>
2023-05-13 20:04:32 +0600

    change: minor change to table

--- src/components/data/EmployeeTable.tsx
+++ src/components/data/EmployeeTable.tsx
@@ -8,9 +8,9 @@ type Props = {
 const EmployeeTable: React.FC<Props> = ({ employees }) => {
   return (
     <div className="md:px-32 py-8 w-full">
-      <div className="shadow overflow-hidden rounded border-b border-gray-200">
+      <div className="shadow-md overflow-hidden rounded-lg border-b border-bnw-blue-gray/20">
         <table className="min-w-full bg-white">
-          <thead className="bg-gray-800 text-white">
+          <thead className="bg-bnw-blue-gray/10 text-bnw-blue-black">
             <tr>
               <th className="w-1/3 text-left py-3 px-4 uppercase font-semibold text-sm">
                 ID
@@ -26,7 +26,7 @@ const EmployeeTable: React.FC<Props> = ({ employees }) => {
               </th>
             </tr>
           </thead>
-          <tbody className="text-zinc-700">
+          <tbody className="text-zinc-700 divide-y divide-bnw-blue-gray/20">
             {employees.map((employee) => (
               <tr key={employee.id}>
                 <td className="w-1/3 text-left py-3 px-4">{employee.id}</td>
