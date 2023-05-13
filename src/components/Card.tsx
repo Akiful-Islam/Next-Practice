74bfdeae4e28e7c45447c3e05aa2a344bd01407e
Arian <arian.islam024@gmail.com>
2023-05-13 13:06:27 +0600

    change: Card completely changed

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,17 +4,37 @@ type Props = {
   children?: React.ReactNode;
   className?: string;
   style?: React.CSSProperties;
+  title?: string;
+  footer?: React.ReactNode;
+  hero?: React.ReactNode;
 };
 
-const Card: React.FC<Props> = ({ children, className, style }) => {
-  let rootDivClass = "flex flex-col justify-center items-center my-2";
+const Card: React.FC<Props> = ({
+  children,
+  className,
+  style,
+  title,
+  hero,
+  footer,
+}) => {
+  let rootDivClass =
+    "relative bg-bnw-blue-white divide-y divide-bnw-blue-gray/10 py-6 px-3 ring-1 ring-bnw-blue-gray/10 shadow-md hover:ring-bnw-blue-accentLight/20 hover:shadow-xl transition-all ease-linear duration-75";
   if (className) {
     rootDivClass += " " + className;
   }
   return (
-    <div className={rootDivClass} style={style}>
-      <div className="!z-5 relative flex flex-col rounded-[20px] bg-clip-border shadow-3xl shadow-current flex flex-col w-full !p-4 3xl:p-![18px] bg-current undefined">
-        {children}
+    <div className="relative min-h-screen flex flex-col items-center justify-center overflow-hidden py-6">
+      <div className={rootDivClass}>
+        {title && (
+          <h2 className="text-bnw-blue-black font-semibold mb-5 mx-10 place-self-auto">
+            {title}
+          </h2>
+        )}
+        <div className="relative h-4/5 flex flex-col items-center justify-center overflow-hidden py-6">
+          {hero}
+          {children}
+        </div>
+        {footer && <div className="pt-8 flex justify-center">{footer}</div>}
       </div>
     </div>
   );
