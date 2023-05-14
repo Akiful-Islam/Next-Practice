467fac0ad3e46280d69a62f0131591107b8befef
Arian <arian.islam024@gmail.com>
2023-05-14 10:01:28 +0600

    change: Employee page changed to now be a separate component and use the new and improved Card component

--- src/app/employees/page.tsx
+++ src/app/employees/page.tsx
@@ -1,36 +1,8 @@
-import Card from "@/components/Card";
-import Button from "@/components/input/Button";
-import Link from "next/link";
+import EmployeesPage from "@/components/page/EmployeesPage";
 import React from "react";
 
 const page = () => {
-  return (
-    <div className="flex flex-col">
-      <h1 className="">Employees</h1>
-      <Card>
-        <div className="employees-root flex flex-col w-80 h-96 justify-center">
-          <Button className="page-button">
-            <Link href="/employees/get">Get All</Link>
-          </Button>
-          <Button type="transparent" className="page-button">
-            <Link href="/employees/get/{id}">Get by Id</Link>
-          </Button>
-          <Button className="page-button">
-            <Link href="/employees/post">Create</Link>
-          </Button>
-          <Button type="transparent" className="page-button">
-            <Link href="/employees/patch/">Update</Link>
-          </Button>
-          <Button className="page-button">
-            <Link href="/employees/delete/">Delete</Link>
-          </Button>
-        </div>
-      </Card>
-      <Button className="place-self-end">
-        <Link href="/">Back</Link>
-      </Button>
-    </div>
-  );
+  return <EmployeesPage />;
 };
 
 export default page;
---
+++ src/components/page/EmployeesPage.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import React from "react";
+import Card from "../Card";
+import Button from "../input/Button";
+import { useRouter } from "next/navigation";
+
+type Props = {};
+
+const EmployeesPage = (props: Props) => {
+  const router = useRouter();
+  return (
+    <div className="flex flex-col">
+      <Card
+        title="Employees"
+        hero={
+          <div className="flex h-full w-full flex-col items-center justify-center overflow-hidden">
+            <Button
+              className="my-1 w-full"
+              title="Get All"
+              onClick={() => router.push("/employees/get")}
+            />
+            <Button
+              className="my-1  w-full"
+              title="Get by Id"
+              onClick={() => router.push("/employees/get/{id}")}
+            />
+            <Button
+              className="my-1 w-full"
+              title="Create"
+              onClick={() => router.push("/employees/post")}
+            />
+            <Button
+              className="my-1 w-full"
+              title="Update"
+              onClick={() => router.push("/employees/patch")}
+            />
+            <Button
+              className="my-1 w-full"
+              title="Delete"
+              onClick={() => router.push("/employees/delete")}
+            />
+          </div>
+        }
+        footer={
+          <Button
+            title="Back"
+            type="transparent"
+            onClick={() => router.push("/")}
+          />
+        }
+      >
+        {" "}
+      </Card>
+    </div>
+  );
+};
+
+export default EmployeesPage;
