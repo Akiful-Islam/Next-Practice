0c6607fb5cd7749d675c5c857df6f22d77ba4b5c
Arian <arian.islam024@gmail.com>
2023-05-15 12:01:40 +0600

    change: clicking on a row routes to that id

--- src/components/data/EmployeeTable.tsx
+++ src/components/data/EmployeeTable.tsx
@@ -2,12 +2,14 @@ import { Employee } from "@/types/Employee";
 import React, { useState } from "react";
 import Button from "../input/Button";
 import UpdaterButtons from "../UpdaterButtons";
+import { useRouter } from "next/navigation";
 
 type Props = {
   employees: Employee[];
 };
 
 const EmployeeTable: React.FC<Props> = ({ employees }) => {
+  const router = useRouter();
   return (
     <div className="md:px-32 py-8 w-full">
       <div className="shadow-md overflow-hidden rounded-lg border-b border-bnw-blue-gray/20">
@@ -23,7 +25,11 @@ const EmployeeTable: React.FC<Props> = ({ employees }) => {
           </thead>
           <tbody className=" text-bnw-blue-black divide-y divide-bnw-blue-gray/20">
             {employees.map((employee) => (
-              <tr key={employee.id} className="">
+              <tr
+                key={employee.id}
+                className="cursor-pointer"
+                onClick={() => router.push("/employees/get/" + employee.id)}
+              >
                 <td className="w-1/3 ">{employee.id}</td>
                 <td className="w-1/3 ">{employee.firstName}</td>
                 <td className="">{employee.lastName}</td>
