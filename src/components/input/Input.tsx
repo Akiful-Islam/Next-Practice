585cf232979addf39b3ba11576f6c85a4a723142
Arian <arian.islam024@gmail.com>
2023-05-16 16:25:30 +0600

    change: minor changes to style

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -22,30 +22,29 @@ const Input: React.FC<Props> = ({
   value,
 }) => {
   return (
-    <div className="relative flex items-center justify-center w-full my-4">
-      <label
-        className="flex items-center justify-center w-full min-w-full"
-        htmlFor={name && name + "Input"}
-      >
-        {label && (
-          <div className="px-2 w-1/3 flex flex-row-reverse">
-            <p className="text-md font-normal">{label + ": "}</p>
-          </div>
-        )}
-        <input
-          className={
-            "w-2/3 h-10 px-2 rounded-md text-lg text-black font-medium border border-bnw-blue-gray/25 transition ease-in duration-75  hover:ring-1 hover:ring-offset-bnw-blue-accentLight/75 hover:shadow-md focus:shadow-lg focus:outline-none focus:ring focus:ring-offset-bnw-blue-accent/75" +
-            " " +
-            className
-          }
-          id={name && name + "Input"}
-          type={type}
-          onChange={onChange}
-          onBlur={onBlur}
-          value={value}
-        />
-      </label>
-    </div>
+    <label
+      className="relative flex items-center justify-center w-full my-4"
+      htmlFor={name && name + "Input"}
+    >
+      {label && (
+        <div className="pl-2 w-1/3 flex flex-row-reverse">
+          <p className="text-md font-normal">{label}</p>
+        </div>
+      )}
+      {label && <p className="text-md font-normal pr-2"> : </p>}
+      <input
+        className={
+          "w-2/3 h-10 px-2 rounded-md text-lg text-black font-medium border border-bnw-blue-gray/25 transition ease-in-out duration-150  hover:ring-1 hover:ring-offset-bnw-blue-accentLight/75 hover:shadow-md focus:shadow-lg focus:outline-none focus:ring focus:ring-offset-bnw-blue-accent/75" +
+          " " +
+          className
+        }
+        id={name && name + "Input"}
+        type={type}
+        onChange={onChange}
+        onBlur={onBlur}
+        value={value}
+      />
+    </label>
   );
 };
 
--- src/components/input/Selector.tsx
+++ src/components/input/Selector.tsx
@@ -27,37 +27,34 @@ const Selector: React.FC<Props> = ({
   value,
 }) => {
   return (
-    <div className="relative flex items-center justify-center w-full my-4">
+    <label
+      className="relative flex items-center justify-center w-full my-2"
+      htmlFor={name && name + "Selector"}
+    >
       {label && (
-        <label
-          className="flex items-center justify-center w-full min-w-full"
-          htmlFor={name && name + "Selector"}
-        >
-          {label && (
-            <div className="px-2 w-1/3 flex flex-row-reverse">
-              <p className="text-md font-normal">{label + ": "}</p>
-            </div>
-          )}
-          <select
-            className={
-              "w-2/3 h-10 px-2 bg-white rounded-md text-lg text-black font-medium border border-bnw-blue-gray/25 transition ease-in duration-75  hover:ring-1 hover:ring-offset-bnw-blue-accentLight/75 hover:shadow-md focus:shadow-lg focus:outline-none focus:ring focus:ring-offset-bnw-blue-accent/75" +
-              " " +
-              className
-            }
-            id={name && name + "Selector"}
-            onChange={onChange}
-            onBlur={onBlur}
-            value={value}
-          >
-            {options.map((option) => (
-              <option key={option.label} value={option.value}>
-                {option.label}
-              </option>
-            ))}
-          </select>
-        </label>
+        <div className="pl-2 w-1/3 flex flex-row-reverse">
+          <p className="text-md font-normal">{label}</p>
+        </div>
       )}
-    </div>
+      {label && <p className="text-md font-normal pr-2"> : </p>}
+      <select
+        className={
+          "w-2/3 h-10 px-2 bg-white rounded-md text-lg text-black font-medium border border-bnw-blue-gray/25 transition ease-in-out duration-150  hover:ring-1 hover:ring-offset-bnw-blue-accentLight/75 hover:shadow-md focus:shadow-lg focus:outline-none focus:ring focus:ring-offset-bnw-blue-accent/75" +
+          " " +
+          className
+        }
+        id={name && name + "Selector"}
+        onChange={onChange}
+        onBlur={onBlur}
+        value={value}
+      >
+        {options.map((option) => (
+          <option className="" key={option.label} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </label>
   );
 };
 
