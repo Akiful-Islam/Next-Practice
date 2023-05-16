1405763afb87809fc387a1607360e35142da2954
Arian <arian.islam024@gmail.com>
2023-05-16 16:25:18 +0600

    change: getall style changed

--- src/components/page/crud/GetAllPaginated.tsx
+++ src/components/page/crud/GetAllPaginated.tsx
@@ -32,38 +32,30 @@ const GetAllPaginated = () => {
 
   return (
     <div className="relative min-h-screen flex flex-col items-center justify-center overflow-hidden py-6">
-      <Card
-        title="All Employees"
-        hero={
-          <QueryForm
-            pageNumber={query.pageNumber}
-            pageSize={query.pageSize}
-            sortBy={query.sortBy}
-            sortDirection={query.sortDirection}
-            setPageNumber={(pageNumber) => setQuery({ ...query, pageNumber })}
-            setPageSize={(pageSize) => setQuery({ ...query, pageSize })}
-            setSortBy={(sortBy) => setQuery({ ...query, sortBy })}
-            setSortDirection={(sortDirection) =>
-              setQuery({ ...query, sortDirection })
-            }
-          />
-        }
-        footer={
-          <div>
-            <Button
-              className="!h-8 !w-16 !text-sm font-medium"
-              title="Add"
-              onClick={() => router.push("employees/post")}
-            />
-            <Button
-              className="!h-8 !w-16 !text-sm font-medium"
-              variant="transparent"
-              title="Back"
-              onClick={() => router.push("/employees")}
-            />
-          </div>
-        }
+      <h1 className="text-6xl font-bold text-bnw-blue-black my-10 transition-colors ease-in-out duration-100 hover:text-bnw-blue-gray">
+        Employees
+      </h1>
+
+      <Button
+        title="Add New Employee"
+        onClick={() => router.push("employees/post")}
       />
+
+      {employees.length > 0 && (
+        <QueryForm
+          pageNumber={query.pageNumber}
+          pageSize={query.pageSize}
+          sortBy={query.sortBy}
+          sortDirection={query.sortDirection}
+          setPageNumber={(pageNumber) => setQuery({ ...query, pageNumber })}
+          setPageSize={(pageSize) => setQuery({ ...query, pageSize })}
+          setSortBy={(sortBy) => setQuery({ ...query, sortBy })}
+          setSortDirection={(sortDirection) =>
+            setQuery({ ...query, sortDirection })
+          }
+        />
+      )}
+
       {employees.length > 0 ? (
         <EmployeeTable employees={employees} />
       ) : (
@@ -71,6 +63,12 @@ const GetAllPaginated = () => {
           Seems like the page is empty
         </p>
       )}
+      <Button
+        className="!h-8 !w-16 !text-sm font-medium"
+        variant="transparent"
+        title="Back"
+        onClick={() => router.push("/employees")}
+      />
     </div>
   );
 };
