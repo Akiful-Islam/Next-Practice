69b78d3f7e152b33041c47ecc9dce4ef565644f6
Arian <arian.islam024@gmail.com>
2023-05-16 15:36:31 +0600

    change: minor changes to style

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -24,17 +24,17 @@ const Input: React.FC<Props> = ({
   return (
     <div className="relative flex items-center justify-center w-full my-4">
       <label
-        className="flex items-center justify-center w-full max-w-full"
+        className="flex items-center justify-center w-full min-w-full"
         htmlFor={name && name + "Input"}
       >
         {label && (
-          <div className="text-md font-medium px-2 min-w-max">
-            {label + ": "}
+          <div className="px-2 w-1/3 flex flex-row-reverse">
+            <p className="text-md font-normal">{label + ": "}</p>
           </div>
         )}
         <input
           className={
-            "w-full max-w-xs h-10 px-2 rounded-md text-lg text-black font-medium border border-bnw-blue-gray/25 transition ease-in duration-75  hover:ring-1 hover:ring-offset-bnw-blue-accentLight/75 hover:shadow-md focus:shadow-lg focus:outline-none focus:ring focus:ring-offset-bnw-blue-accent/75" +
+            "w-2/3 h-10 px-2 rounded-md text-lg text-black font-medium border border-bnw-blue-gray/25 transition ease-in duration-75  hover:ring-1 hover:ring-offset-bnw-blue-accentLight/75 hover:shadow-md focus:shadow-lg focus:outline-none focus:ring focus:ring-offset-bnw-blue-accent/75" +
             " " +
             className
           }
