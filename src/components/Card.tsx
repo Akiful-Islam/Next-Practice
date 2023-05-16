7a789cb423f579b3ad0d8f304ef11bb368e33d94
Arian <arian.islam024@gmail.com>
2023-05-16 15:36:57 +0600

    refactor: minor changes

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -17,13 +17,14 @@ const Card: React.FC<Props> = ({
   hero,
   footer,
 }) => {
-  let rootDivClass =
-    "relative bg-bnw-blue-white rounded-md divide-y divide-bnw-blue-gray/10 py-6 px-3 ring-1 ring-bnw-blue-gray/10 shadow-md hover:ring-bnw-blue-accentLight/20 hover:shadow-xl transition-all ease-linear duration-75";
-  if (className) {
-    rootDivClass += " " + className;
-  }
   return (
-    <div className={rootDivClass}>
+    <div
+      className={
+        "relative bg-bnw-blue-white rounded-md divide-y divide-bnw-blue-gray/10 py-6 px-3 ring-1 ring-bnw-blue-gray/10 shadow-md hover:ring-bnw-blue-accentLight/20 hover:shadow-xl transition-all ease-linear duration-75" +
+        " " +
+        className
+      }
+    >
       {title && (
         <h2 className="text-bnw-blue-black font-semibold mb-5 mx-10 place-self-auto">
           {title}
