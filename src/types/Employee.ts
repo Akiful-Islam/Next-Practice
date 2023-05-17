8c87cc44994097e42ca4a266f90dd40ab99845ee
Arian <arian.islam024@gmail.com>
2023-05-17 21:37:59 +0600

    refactor: Employee types revamped

--- src/types/Employee.ts
+++ src/types/Employee.ts
@@ -1,8 +1,14 @@
-export type Employee = {
-  id: number;
+type Employee = {
   firstName: string;
   lastName: string;
   email: string;
   phoneNumber: string;
   position: "Developer" | "Manager" | "HR" | "QA";
 };
+
+export type ResponseEmployee = Employee & {
+  id: number;
+};
+
+export type PostEmployee = Employee;
+export type PatchEmployee = Partial<Employee>;
