8117a93fa54f134d5fa9cf5b83225688a9235642
Arian <arian.islam024@gmail.com>
2023-05-17 16:18:50 +0600

    change: updated employee type to match backend

--- src/types/Employee.ts
+++ src/types/Employee.ts
@@ -3,37 +3,6 @@ export type Employee = {
   firstName: string;
   lastName: string;
   email: string;
+  phone: string;
+  position: "Developer" | "Manager" | "HR" | "QA";
 };
-
-export const dummyEmployees: Employee[] = [
-  {
-    id: 1,
-    firstName: "John",
-    lastName: "Doe",
-    email: "john.doe@example.com",
-  },
-  {
-    id: 2,
-    firstName: "Jane",
-    lastName: "Doe",
-    email: "jane.doe@example.com",
-  },
-  {
-    id: 3,
-    firstName: "Bob",
-    lastName: "Smith",
-    email: "bob.smith@example.com",
-  },
-  {
-    id: 4,
-    firstName: "Alice",
-    lastName: "Johnson",
-    email: "alice.johnson@example.com",
-  },
-  {
-    id: 5,
-    firstName: "Charlie",
-    lastName: "Brown",
-    email: "charlie.brown@example.com",
-  },
-];
