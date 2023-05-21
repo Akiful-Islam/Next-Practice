db9799a0e1ce4b7091f8ea9bfee28b58fcb7c8cf
Arian <arian.islam024@gmail.com>
2023-05-21 12:24:42 +0600

    update: delete component updated

--- src/components/page/crud/DeleteById.tsx
+++ src/components/page/crud/DeleteById.tsx
@@ -1,69 +1,32 @@
 "use client";
 
-import { deleteEmployee } from "@/services/EmployeeServices";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Card from "@/components/Card";
 import { useRouter } from "next/navigation";
 import Button from "@/components/input/Button";
+import { ResponseEmployee } from "@/types/Employee";
 
 type Props = {
-  routeId: string;
+  deletedEmployee: ResponseEmployee;
 };
 
-const DeleteById: React.FC<Props> = ({ routeId }) => {
+const DeleteById: React.FC<Props> = ({ deletedEmployee }) => {
   const router = useRouter();
 
-  const [employeeFound, setEmployeeFound] = useState(false);
-  const [error, setError] = useState<string | null>(null);
-
-  const fetchDeleteById = async (routeId: string) => {
-    const parsedId = parseInt(routeId);
-    if (isNaN(parsedId)) {
-      setError(`Invalid route "${routeId}". Enter a valid number.`);
-      return;
-    }
-
-    if (parsedId < 1) {
-      setError(`Invalid route "${routeId}". Id starts from 1.`);
-      return;
-    }
-    const res = await deleteEmployee(parsedId);
-    if (res) {
-      setError(`${res.code} - ${res.errorMessage}`);
-    } else {
-      setEmployeeFound(true);
-    }
-  };
-
-  useEffect(() => {
-    fetchDeleteById(routeId);
-  }, []);
-
-  let title;
-  if (employeeFound) {
-    title = `Employee Deleted`;
-  } else {
-    title = "Invalid Employee ID";
-  }
-
   return (
-    <div className="relative min-h-screen flex flex-col items-center justify-center overflow-hidden py-6">
+    <div className="relative min-h-max flex flex-col items-center justify-center overflow-hidden py-6">
       <Card
-        title={title}
+        title={`Deleted Employee: ${deletedEmployee.id}`}
         hero={
-          <div className="response">
-            {employeeFound ? (
-              <div className="ok-response">
-                <p>Employee Successfully Deleted with id: {routeId}</p>
-              </div>
-            ) : (
-              error && (
-                <div>
-                  <p>Error Occured :(</p>
-                  <p>{error}</p>
-                </div>
-              )
-            )}
+          <div className="animate-pulse">
+            <h3 className="overflow-x-auto text-xl text-green-600 my-4">{`Successfully Deleted ${deletedEmployee.firstName} ${deletedEmployee.lastName}`}</h3>
+            <h4 className="overflow-x-auto text-lg text-green-500 my-4">{`Position: ${deletedEmployee.position}`}</h4>
+            <p className="overflow-x-auto font-normal text-base text-green-400 my-4">
+              {`Phone: ${deletedEmployee.phoneNumber}`}
+            </p>
+            <p className="overflow-x-auto font-normal text-base text-green-400 my-4">
+              {`Email: ${deletedEmployee.email}`}
+            </p>
           </div>
         }
         footer={
